18ac2bef19efba2dc3b59066a905366bb27c41d0
yee0802 <NovusAa12@proton.me>
2024-10-28 13:54:24 +0000

    feat: add WordAnalyser class

---
+++ src/main/java/WordAnalyser.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class WordAnalyser {
+    public String[] findLongestWords(String text) {
+        // TODO: Implement the logic to find the longest word(s) in the given text
+        return new String[]{""};
+    }
+
+    public Map<Character, Integer> calculateLetterFrequency(String text) {
+        // TODO: Implement the logic to calculate the frequency of each letter in the given text
+        return null;
+    }
+}
