9be7eeb172fc2759dbed38dc254e2e79144e6220
yee0802 <NovusAa12@proton.me>
2024-10-28 11:14:40 +0000

    feat: add Compass class

---
+++ src/main/java/Compass.java
@@ -0,0 +1,20 @@
+public class Compass {
+    public enum Direction {
+        LEFT,
+        RIGHT
+    }
+
+    public enum Point {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+    public Point point;
+
+
+    public Point rotate(Point point, Direction direction) {
+        return null;
+    }
+}
