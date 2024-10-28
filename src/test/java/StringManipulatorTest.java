6a954980162f0ad64e1ab10e9d7199944f4ae207
yee0802 <NovusAa12@proton.me>
2024-10-28 13:22:59 +0000

    test: add testReverseString unit test

---
+++ src/test/java/StringManipulatorTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringManipulatorTest {
+
+    @Test
+    @DisplayName("StringManipulator: reverseString should reverse string in correct format")
+    void testReverseString() {
+        StringManipulator stringManipulator = new StringManipulator();
+        var inputStr1 = "reverse me!";
+        var expectedOutput1 = "!em esrever";
+
+        var inputStr2 = "Hello World";
+        var expectedOutput2 = "dlroW oellH";
+
+        var inputStr3 = "!desrever yllufss3ccus am I";
+        var expectedOutput3 = "I am succ3ssfully reversed!";
+
+
+        var result1 = stringManipulator.reverseString(inputStr1);
+        var result2 = stringManipulator.reverseString(inputStr2);
+        var result3 = stringManipulator.reverseString(inputStr3);
+
+        assertAll("Grouped Assertions for reverseString method",
+                () -> assertEquals(expectedOutput1, result1),
+                () -> assertEquals(expectedOutput2, result2),
+                () -> assertEquals(expectedOutput3, result3)
+        );
+
+    }
+}
