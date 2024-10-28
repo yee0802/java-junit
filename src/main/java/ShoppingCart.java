e4fe61bfa9390c7584dc3bd4a6e63ccced8ae2a4
yee0802 <NovusAa12@proton.me>
2024-10-28 16:03:25 +0000

    feat: add ShoppingCart class

---
+++ src/main/java/ShoppingCart.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ShoppingCart {
+    private Map<String, Double> items = new HashMap<>();
+
+    public void addItem(String item) {
+        // TODO: implement logic for adding an item to ShoppingCart
+    }
+
+    public void calculateTotalPrice() {
+        // TODO: implement logic for calculating total price of all items in ShoppingCart
+    }
+}
