c567d52fdb0c76623469605b5e6709d912299c81
yee0802 <NovusAa12@proton.me>
2024-10-28 11:28:53 +0000

    feat: implement logic for Compass rotate method

--- src/main/java/Compass.java
+++ src/main/java/Compass.java
@@ -3,18 +3,41 @@ public class Compass {
         LEFT,
         RIGHT
     }
-
     public enum Point {
         NORTH,
         EAST,
         SOUTH,
         WEST
     }
-
     public Point point;
 
-
     public Point rotate(Point point, Direction direction) {
-        return null;
+        Point result = null;
+
+        if (direction == Direction.LEFT) {
+            if (point == Point.NORTH) {
+                result = Point.WEST;
+            } else if (point == Point.EAST) {
+                result = Point.NORTH;
+            } else if (point == Point.SOUTH) {
+                result = Point.EAST;
+            } else if (point == Point.WEST) {
+                result = Point.SOUTH;
+            }
+        }
+
+        if (direction == Direction.RIGHT) {
+            if (point == Point.NORTH) {
+                result = Point.EAST;
+            } else if (point == Point.EAST) {
+                result = Point.SOUTH;
+            } else if (point == Point.SOUTH) {
+                result = Point.WEST;
+            } else if (point == Point.WEST) {
+                result = Point.NORTH;
+            }
+        }
+
+        return result;
     }
 }
