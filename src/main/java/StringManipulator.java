d8e7018fd1a09aa7b5f34c2f08c93472cda8f548
yee0802 <NovusAa12@proton.me>
2024-10-28 13:15:49 +0000

    feat: add StringManipulator class

---
+++ src/main/java/StringManipulator.java
@@ -0,0 +1,11 @@
+public class StringManipulator {
+    public String reverseString(String input) {
+        // TODO: Implement the logic to reverse the input string
+        return "";
+    }
+
+    public boolean isPalindrome(String input) {
+        // TODO: Implement the logic to check if the input string is a palindrome
+        return false;
+    }
+}
